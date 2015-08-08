5fb6b0113c8e1bea17bcf107dea1809ee95e43ef
Rajesh Kumar <rajeshkankran@gmail.com>
2015-08-08 16:02:45 +0530

    Update AppConfig.java

--- src/main/java/com/tupa/app/config/AppConfig.java
+++ src/main/java/com/tupa/app/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan(basePackages={"com.trupay.app.controller"})
+@ComponentScan(basePackages={"com.tupa.app.controller"})
 public class AppConfig {
 
 }
